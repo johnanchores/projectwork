2a2827b3a49022a7a0099da2813babcc2b16d8e3
Biziol <lombizio05@gmail.com>
2025-10-20 23:41:44 +0200

    Modifiche generali implementazione dashboard

--- src/main/java/dev2426/ITSProjectWork/controller/DashboardController.java
+++ src/main/java/dev2426/ITSProjectWork/controller/DashboardController.java
@@ -1,13 +1,17 @@
 package dev2426.ITSProjectWork.controller;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import dev2426.ITSProjectWork.model.Azienda;
 import dev2426.ITSProjectWork.model.Tirocinio;
+import dev2426.ITSProjectWork.model.TirocinioGUI;
 import dev2426.ITSProjectWork.services.AziendaService;
 import dev2426.ITSProjectWork.services.CompetenzaService;
 import dev2426.ITSProjectWork.services.TirocinioService;
@@ -28,7 +32,16 @@ public class DashboardController {
 	@GetMapping("/dashboard")
 	public String showHome(Model model) {
 		List<Tirocinio> listaTir = tServ.getAll();
-		model.addAttribute("tirocini", listaTir);
+		List<TirocinioGUI> listaTirGUI = new ArrayList<>();
+
+		for (Tirocinio t : listaTir) {
+			Optional<Azienda> azienda = aServ.find(t.getId_azienda());
+
+			listaTirGUI.add(new TirocinioGUI(azienda.orElse(null), t));
+
+		}
+
+		model.addAttribute("tirocini", listaTirGUI);
 		return "/private/dashboard";
 	}
 
--- src/main/java/dev2426/ITSProjectWork/services/AziendaService.java
+++ src/main/java/dev2426/ITSProjectWork/services/AziendaService.java
@@ -11,36 +11,34 @@ import dev2426.ITSProjectWork.repository.AziendeRepository;
 
 @Service
 public class AziendaService {
-	
+
 	@Autowired
 	private AziendeRepository repo;
-	
-	public List<Azienda> getAll(){
+
+	public List<Azienda> getAll() {
 		return repo.findAll();
 	}
-	
+
 	public void insert(Azienda nuova) {
 		repo.save(nuova);
 	}
-	
+
 	public void deleteAzienda(long idCancella) {
 		repo.deleteById(idCancella);
 	}
-	
-	public Optional<Azienda> find(long idCerca){
+
+	public Optional<Azienda> find(long idCerca) {
 		return repo.findById(idCerca);
 	}
-	
+
 	public void update(Azienda nuova, long idUpdate) {
 		Optional<Azienda> found = repo.findById(idUpdate);
 		Azienda a = found.get();
 		a.setIdAzienda(nuova.getIdAzienda());
-		a.setIdTirocinio(nuova.getIdTirocinio());
 		a.setLocalita(nuova.getLocalita());
 		a.setNome(nuova.getNome());
 		a.setPartitaIva(nuova.getPartitaIva());
 		repo.save(a);
 	}
-	
 
 }
