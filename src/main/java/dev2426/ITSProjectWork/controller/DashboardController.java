52482ad027411546c1d47d44e3e4651c6bfe37b6
johnanchores <jonathananchores@gmail.com>
2025-10-21 11:57:30 +0200

    SDA

--- src/main/java/dev2426/ITSProjectWork/controller/DashboardController.java
+++ src/main/java/dev2426/ITSProjectWork/controller/DashboardController.java
@@ -15,63 +15,61 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import dev2426.ITSProjectWork.model.Candidatura;
 import dev2426.ITSProjectWork.model.Competenza;
-import dev2426.ITSProjectWork.model.Azienda;
 import dev2426.ITSProjectWork.model.Tirocinio;
 import dev2426.ITSProjectWork.model.TirocinioGUI;
 import dev2426.ITSProjectWork.model.Utente;
-import dev2426.ITSProjectWork.services.AziendaService;
 import dev2426.ITSProjectWork.services.CandidaturaService;
-import dev2426.ITSProjectWork.services.CompetenzaService;
 import dev2426.ITSProjectWork.services.TirocinioService;
 import dev2426.ITSProjectWork.services.UtentiService;
 
 @Controller
 public class DashboardController {
 
-	@Autowired
-	private AziendaService aServ;
+    @Autowired
+    private TirocinioService tServ;
+    
+    @Autowired
+    private UtentiService uServ;
+    
+    @Autowired 
+    private CandidaturaService caServ;
 
-	@Autowired
-	private UtentiService uServ;
 
-	@Autowired
-	private CandidaturaService cServ;
+    @GetMapping("/dashboard")
+    public String showHome(Model model) {
+        
+        List<Tirocinio> listaTir = tServ.getAll();
+        
+        List<TirocinioGUI> listaCompleta = new ArrayList<>();
+        
+        for (Tirocinio t : listaTir) {
+            TirocinioGUI tg = new TirocinioGUI();
+            
+            tg.setId_tirocinio(t.getIdTirocinio());
+            tg.setMansione(t.getMansione());
+            tg.setDurata(t.getDurata());
+            tg.setDescrizione(t.getDescrizione());
+            
+            
+            if (t.getAzienda() != null) {
+                tg.setNomeAzienda(t.getAzienda().getNome());
+            }
 
-	@Autowired
-	private TirocinioService tServ;
+            if (t.getCompetenze() != null) {
+               
+                List<String> nomiCompetenze = t.getCompetenze().stream()
+                                                .map(Competenza::getNome)
+                                                .toList();
+                tg.setNome_competenza(nomiCompetenze); 
+            }
+            
+            listaCompleta.add(tg);
+        }
 
-	@Autowired
-	private CompetenzaService coServ;
-
-	@GetMapping("/dashboard")
-	public String showHome(Model model) {
-		List<Tirocinio> listaTir = tServ.getAll();
-		List<Azienda> listaA = aServ.getAll();
-		List<Competenza> listaCo = coServ.getAll();
-		List<TirocinioGUI> listaCompleta = new ArrayList<>();
-		for (Tirocinio t : listaTir) {
-			TirocinioGUI tg = new TirocinioGUI();
-			for (Azienda a : listaA) {
-
-				if (t.getId_azienda() == a.getIdAzienda()) {
-
-					tg.setDescrizione(t.getDescrizione());
-					tg.setDurata(t.getDurata());
-					tg.setMansione(t.getMansione());
-					tg.setNomeAzienda(a.getNome());
-					tg.setId_tirocinio(t.getIdTirocinio());
-
-				}
-
-			}
-		
-			listaCompleta.add(tg);
-
-		}
-		model.addAttribute("tirocini", listaCompleta);
-		return "/private/dashboard";
-	}
-
+        model.addAttribute("tirocini", listaCompleta);
+        return "private/dashboard";
+    }
+    
 	@PostMapping("/candidature")
 	public String Candidatura(@ModelAttribute("Candidatura") Candidatura newCand, Principal p,
 			@RequestParam Long id_tirocinio) {
@@ -79,16 +77,17 @@ public class DashboardController {
 		if (p == null) {
 			return "redirect:/login";
 		}
-		String emailUtenteLoggato = p.getName();
-		Optional<Utente> userOptional = uServ.findByEmail(emailUtenteLoggato);
+		
+		Optional<Utente> userOptional = uServ.findByEmail(p.getName());
+		
 		if (userOptional.isPresent()) {
 			Utente user = userOptional.get();
 			newCand.setId_utente(user.getIdUtente());
+			newCand.setIdTirocinio(id_tirocinio);
+			newCand.setStato(0); 
+			caServ.insert(newCand);
 		}
-		newCand.setIdTirocinio(id_tirocinio);
-		newCand.setStato(0);
-		cServ.insert(newCand);
-		return "redirect:/home";
+		
+		return "redirect:/dashboard";
 	}
-
 }
--- src/main/java/dev2426/ITSProjectWork/model/Tirocinio.java
+++ src/main/java/dev2426/ITSProjectWork/model/Tirocinio.java
@@ -1,64 +1,77 @@
 package dev2426.ITSProjectWork.model;
 
 import java.util.List;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tirocinio")
 public class Tirocinio {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id_tirocinio;
-	private String mansione;
-	private String durata;
-	private long id_azienda;
-	private String descrizione; 
-	
-	public long getIdTirocinio() {
-		return id_tirocinio;
-	}
-	public void setIdTirocinio(long idTirocinio) {
-		this.id_tirocinio = idTirocinio;
-	}
-	public String getMansione() {
-		return mansione;
-	}
-	public void setMansione(String mansione) {
-		this.mansione = mansione;
-	}
-	public String getDurata() {
-		return durata;
-	}
-	public void setDurata(String durata) {
-		this.durata = durata;
-	}
-	public Tirocinio(long idTirocinio, String mansione, String durata, long idAzienda, String descrizione) {
-		this.id_tirocinio = idTirocinio;
-		this.mansione = mansione;
-		this.durata = durata;
-		this.id_azienda = idAzienda;
-		this.descrizione = descrizione;
-	}
-	public Tirocinio() {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id_tirocinio;
+    private String mansione;
+    private String durata;
+    private String descrizione;
 
-	}
-	public long getId_azienda() {
-		return id_azienda;
-	}
-	public void setId_azienda(long id_azienda) {
-		this.id_azienda = id_azienda;
-	}
-	public String getDescrizione() {
-		return descrizione;
-	}
-	public void setDescrizione(String descrizione) {
-		this.descrizione = descrizione;
-	}
-	
+    @ManyToOne
+    @JoinColumn(name = "id_azienda") 
+    private Azienda azienda;
+
+    @ManyToMany
+    @JoinTable(
+        name = "tirocinio_competenza", 
+        joinColumns = @JoinColumn(name = "id_tirocinio"), 
+        inverseJoinColumns = @JoinColumn(name = "id_competenza")
+    )
+    private List<Competenza> competenze;
+
+    public long getIdTirocinio() {
+        return id_tirocinio;
+    }
+    public void setIdTirocinio(long idTirocinio) {
+        this.id_tirocinio = idTirocinio;
+    }
+    public String getMansione() {
+        return mansione;
+    }
+    public void setMansione(String mansione) {
+        this.mansione = mansione;
+    }
+    public String getDurata() {
+        return durata;
+    }
+    public void setDurata(String durata) {
+        this.durata = durata;
+    }
+    public String getDescrizione() {
+        return descrizione;
+    }
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+    public Azienda getAzienda() {
+        return azienda;
+    }
+    public void setAzienda(Azienda azienda) {
+        this.azienda = azienda;
+    }
+    public List<Competenza> getCompetenze() {
+        return competenze;
+    }
+    public void setCompetenze(List<Competenza> competenze) {
+        this.competenze = competenze;
+    }
+    
+    public Tirocinio() {
+
+    }
 }
