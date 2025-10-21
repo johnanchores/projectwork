80ae391def2b6e3e1c27bb3cb7b1dfa2fde9b5dd
johnanchores <jonathananchores@gmail.com>
2025-10-21 12:25:22 +0200

    sdd

--- src/main/java/dev2426/ITSProjectWork/controller/DashboardController.java
+++ src/main/java/dev2426/ITSProjectWork/controller/DashboardController.java
@@ -15,61 +15,62 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import dev2426.ITSProjectWork.model.Candidatura;
 import dev2426.ITSProjectWork.model.Competenza;
+import dev2426.ITSProjectWork.model.Azienda;
 import dev2426.ITSProjectWork.model.Tirocinio;
 import dev2426.ITSProjectWork.model.TirocinioGUI;
 import dev2426.ITSProjectWork.model.Utente;
+import dev2426.ITSProjectWork.services.AziendaService;
 import dev2426.ITSProjectWork.services.CandidaturaService;
+import dev2426.ITSProjectWork.services.CompetenzaService;
 import dev2426.ITSProjectWork.services.TirocinioService;
 import dev2426.ITSProjectWork.services.UtentiService;
 
 @Controller
 public class DashboardController {
 
-    @Autowired
-    private TirocinioService tServ;
-    
-    @Autowired
-    private UtentiService uServ;
-    
-    @Autowired 
-    private CandidaturaService caServ;
+	@Autowired
+	private AziendaService aServ;
 
+	@Autowired
+	private UtentiService uServ;
 
-    @GetMapping("/dashboard")
-    public String showHome(Model model) {
-        
-        List<Tirocinio> listaTir = tServ.getAll();
-        
-        List<TirocinioGUI> listaCompleta = new ArrayList<>();
-        
-        for (Tirocinio t : listaTir) {
-            TirocinioGUI tg = new TirocinioGUI();
-            
-            tg.setId_tirocinio(t.getIdTirocinio());
-            tg.setMansione(t.getMansione());
-            tg.setDurata(t.getDurata());
-            tg.setDescrizione(t.getDescrizione());
-            
-            
-            if (t.getAzienda() != null) {
-                tg.setNomeAzienda(t.getAzienda().getNome());
-            }
+	@Autowired
+	private CandidaturaService cServ;
 
-            if (t.getCompetenze() != null) {
-               
-                List<String> nomiCompetenze = t.getCompetenze().stream()
-                                                .map(Competenza::getNome)
-                                                .toList();
-                tg.setNome_competenza(nomiCompetenze); 
-            }
-            
-            listaCompleta.add(tg);
-        }
+	@Autowired
+	private TirocinioService tServ;
 
-        model.addAttribute("tirocini", listaCompleta);
-        return "private/dashboard";
-    }
-    
+	@Autowired
+	private CompetenzaService coServ;
+
+	@GetMapping("/dashboard")
+	public String showHome(Model model) {
+		List<Tirocinio> listaTir = tServ.getAll();
+		List<Azienda> listaA = aServ.getAll();
+		List<TirocinioGUI> listaCompleta = new ArrayList<>();
+		for (Tirocinio t : listaTir) {
+			TirocinioGUI tg = new TirocinioGUI();
+			for (Azienda a : listaA) {
+
+				if (t.getId_azienda() == a.getIdAzienda()) {
+
+					tg.setDescrizione(t.getDescrizione());
+					tg.setDurata(t.getDurata());
+					tg.setMansione(t.getMansione());
+					tg.setNomeAzienda(a.getNome());
+					tg.setId_tirocinio(t.getIdTirocinio());
+
+				}
+
+			}
+		
+			listaCompleta.add(tg);
+
+		}
+		model.addAttribute("tirocini", listaCompleta);
+		return "/private/dashboard";
+	}
+
 	@PostMapping("/candidature")
 	public String Candidatura(@ModelAttribute("Candidatura") Candidatura newCand, Principal p,
 			@RequestParam Long id_tirocinio) {
@@ -77,17 +78,16 @@ public class DashboardController {
 		if (p == null) {
 			return "redirect:/login";
 		}
-		
-		Optional<Utente> userOptional = uServ.findByEmail(p.getName());
-		
+		String emailUtenteLoggato = p.getName();
+		Optional<Utente> userOptional = uServ.findByEmail(emailUtenteLoggato);
 		if (userOptional.isPresent()) {
 			Utente user = userOptional.get();
 			newCand.setId_utente(user.getIdUtente());
-			newCand.setIdTirocinio(id_tirocinio);
-			newCand.setStato(0); 
-			caServ.insert(newCand);
 		}
-		
-		return "redirect:/dashboard";
+		newCand.setIdTirocinio(id_tirocinio);
+		newCand.setStato(0);
+		cServ.insert(newCand);
+		return "redirect:/home";
 	}
+
 }
