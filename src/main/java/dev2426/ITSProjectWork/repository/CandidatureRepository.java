88806776cb7455632896cd59610996447db847e8
johnanchores <jonathananchores@gmail.com>
2025-10-21 14:44:28 +0200

    dsadsa

--- src/main/java/dev2426/ITSProjectWork/repository/CandidatureRepository.java
+++ src/main/java/dev2426/ITSProjectWork/repository/CandidatureRepository.java
@@ -13,17 +13,21 @@ import dev2426.ITSProjectWork.model.CandidaturaGUI;
 public interface CandidatureRepository extends JpaRepository<Candidatura, Long> {
 	
 	@Query("SELECT new dev2426.ITSProjectWork.model.CandidaturaGUI(" +
-	           "c.id_candidatura, u.nome, u.cognome, t.mansione, a.nome, " +
-	           "CASE c.stato " +
-	           "  WHEN 0 THEN 'In attesa' " +
-	           "  WHEN 1 THEN 'Approvata' " +
-	           "  WHEN 2 THEN 'Rifiutata' " +
-	           "  ELSE 'Sconosciuto' " +
-	           "END) " +
-	           "FROM Candidatura c, Utente u, Tirocinio t, Azienda a " +
-	           "WHERE c.id_utente = u.id_utente " + 
-	           "AND c.id_tirocinio = t.id_tirocinio " +
-	           "AND t.id_azienda = a.id_azienda") 
+	           "   c.id_candidatura, " +        
+	           "   u.nome, " +                  
+	           "   u.cognome, " +               
+	           "   t.mansione, " +              
+	           "   a.nome, " +                  
+	           "   CASE c.stato " +
+	           "       WHEN 0 THEN 'In attesa' " +
+	           "       WHEN 1 THEN 'Approvata' " +
+	           "       WHEN 2 THEN 'Rifiutata' " +
+	           "       ELSE 'Sconosciuto' " +
+	           "   END) " +
+	           "FROM Candidatura c " +          
+	           "JOIN c.utente u " +              
+	           "JOIN c.tirocinio t " +           
+	           "JOIN t.azienda a")             
 	    List<CandidaturaGUI> findAllAsGUI();
 
 }
